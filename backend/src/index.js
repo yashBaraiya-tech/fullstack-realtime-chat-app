aa89ba585d27f3192ee9c0c4ec9a1238accfcf43
Yash Baraiya <yashbaraiya420@gmail.com>
2026-07-01 00:14:04 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,11 +31,9 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve();
-
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
